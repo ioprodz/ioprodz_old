6d931d927b0f0dcb28c66b1c9d3019f24ed5dbaa
badido39@gmail.com <badido39@gmail.com>
2022-10-26 01:05:50 +0100

    add error enum & status code

--- hoggar/src/app.ts
+++ hoggar/src/app.ts
@@ -1,9 +1,10 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
 
 import cors from "cors";
 import bodyParser from "body-parser";
 
 import { addSubscription } from "../services";
+import { object, string } from "yup";
 
 const app: Express = express();
 
@@ -13,16 +14,45 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.get("/", (_: Request, res: Response) => {
   res.send("HELLO WORLD!👋👋");
 });
-app.post("/subscription", async (req: Request, res: Response) => {
-  const { email, source } = req.body;
-  const added = await addSubscription(email, source)
-    .then((response: any) => {
-      return response;
-    })
-    .catch((e: any) => {
-      return e.message;
-    });
-  res.status(201).send(added);
+const subscriberSchema = object({
+  body: object({
+    email: string().email().required(),
+    source: string(),
+  }),
+  params: object({
+    id: string(),
+  }),
 });
+
+//middleware for validating requests
+const validate =
+  (schema: any) => async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema.validate({
+        body: req.body,
+        query: req.query,
+        params: req.params,
+      });
+      return next();
+    } catch (err) {
+      return res.status(500).json({ type: err.name, message: err.message });
+    }
+  };
+
+app.post(
+  "/subscription",
+  validate(subscriberSchema),
+  async (req: Request, res: Response) => {
+    const { email, source } = req.body;
+    const added = await addSubscription(email, source)
+      .then((response: any) => {
+        return response;
+      })
+      .catch((e: any) => {
+        return { error: e.message };
+      });
+    res.status(201).send(added);
+  }
+);
 
 export default app;
--- hoggar/src/index.ts
+++ hoggar/src/index.ts
@@ -1,6 +1,5 @@
 import app from "./app";
 import { hostname, port } from "./config";
-import { number, object, string } from "yup";
 
 app.listen(port, hostname, () => {
   console.log(`⚡️[server]: Server is running at http://${hostname}:${port}`);
