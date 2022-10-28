8da9894aa484bd60eedd8437811e9ff94a8f8c49
badido39@gmail.com <badido39@gmail.com>
2022-10-28 23:30:50 +0100

    add gitHub clientID

--- hoggar/src/auth/routes.ts
+++ hoggar/src/auth/routes.ts
@@ -1,9 +1,13 @@
 import { NextFunction, Request, Response, Router } from "express";
 import {} from "./business";
 
+const CLIENT_ID = "59bacf304331ecf0c890";
+
 const auth = Router();
 
 auth.get("/github", () => {
+  "https://github.com/login/oauth/authorize?client_id=" + CLIENT_ID;
+
   return null;
 });
 auth.post("/github/callback", () => {
