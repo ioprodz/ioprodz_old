5ef276fcc635eed8cd7620e402351fb59ede65d5
badido39@gmail.com <badido39@gmail.com>
2022-10-29 00:04:17 +0100

    add route

--- hoggar/src/auth/business.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
-import { object, string } from "yup";
-import { prisma } from "../db/prisma";
-
-enum Errors {
-  DUPLICATE_ENTRY = "DUPLICATE_ENTRY",
-}
