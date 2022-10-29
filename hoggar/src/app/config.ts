fca9106fd312b4e541b06b50f6aa5390724f6e3d
badido39@gmail.com <badido39@gmail.com>
2022-10-29 20:48:00 +0100

    fix conflict

--- hoggar/src/app/config.ts
+++ hoggar/src/app/config.ts
@@ -4,4 +4,5 @@ dotenv.config();
 
 export const port = parseInt(process.env.PORT || "8000", 10);
 export const hostname = process.env.HOSTNAME || "localhost";
-export const dburl = process.env.DATABASE_URL || "file:data/db.sqlite";
+export const dburl = process.env.DATABASE_URL || "file:data/dev.db";
+export const CLIENT_ID = process.env.CLIENT_ID || "59bacf304331ecf0c890";
