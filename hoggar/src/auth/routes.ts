c25dd88a88955b45767f384b06e3c27b6498e224
badido39@gmail.com <badido39@gmail.com>
2022-10-29 22:08:25 +0100

    add logic

--- hoggar/src/auth/routes.ts
+++ hoggar/src/auth/routes.ts
@@ -1,23 +1,44 @@
 import { NextFunction, Request, Response, Router } from "express";
 
-const CLIENT_ID = "59bacf304331ecf0c890";
+const CLIENT_ID = "6bacd8943692208dfe77";
 
 const auth = Router();
 
 auth.get("/github", (_: Request, res: Response, next: NextFunction) => {
   const url = "https://github.com/login/oauth/authorize?client_id=" + CLIENT_ID;
   try {
-    res.status(201).send(url);
+    res.redirect(url);
   } catch (e: any) {
     next({ ...e, status: 404 });
   }
   return;
 });
 
-auth.post("/github/callback", () => {
-  return null;
-});
+auth.get(
+  "/github/callback",
+  (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.query);
+    /*after get this code , send post to get acces token to 
+    POST https://github.com/login/oauth/access_token
+    after
+    send get user with acces_token
+    
+    */
+ 
+    return req;
+
+    /*
+    1- seacrch for user and get user data 
+    2- search fro user in db 
+    4- create user if not exist 
+    3- token & refrech token 
+
+    */
+  }
+);
 auth.post("/refresh", () => {
+
+
   return null;
 });
 
