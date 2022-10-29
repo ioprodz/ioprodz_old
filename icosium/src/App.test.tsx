619cd0d1bca0862b32c7c91fd8aa5cc2b718d8b6
Osman Kalache <osminosm@gmail.com>
2022-10-29 18:18:07 +0200

    chore(project): split package.json dependencies (#41)
    chore(project): fixed commands, updated ci, docs
    
    chore(project): fixed linting commands + doc
    
    chore(project): uniform commands + doc
    
    chore(project): split ci by app + adjust commands
    
    chore(project): lint & test fixes for icosium
    
    fix(ci): fixed invalid job names
    
    chore(project): regrouped ci, parralel build, on demand

--- icosium/src/App.test.tsx
+++ icosium/src/App.test.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { I18nextProvider } from "react-i18next";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import i18n from "./lang/i18n";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test("renders learn react link", () => {
+  render(
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+  );
+  const linkElement = screen.getByText(/IOPRODZ/i);
   expect(linkElement).toBeInTheDocument();
 });
